c55b147bfaa0e47df8d95cdb148ee45bfc389f34
lht818 <lht818@R2045PC14.ad.utulsa.edu>
2017-09-19 13:39:23 -0500

    Change-Id: I876d8a4625c7c79848323d0bbcb71716d2ddfe54 Signed-off-by: lht818 <lht818@R2045PC14.ad.utulsa.edu>

---
+++ lab4/debug.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class debug{
+	
+public static void main(String[] args) 
+		{
+			Counter counter = new Counter();
+			counter.count();
+			System.out.println("Counted " + counter.getResult());
+		}
+
+
+
+}
---
+++ lab4/test2.java
@@ -0,0 +1,17 @@
+package Test;
+public class test2 
+{
+	
+		private int result = 0;
+
+		public int getResult() {
+			return result;
+		}
+
+		public void count() {
+			for (int i = 0; i < 100; i++) {
+				result += i + 1;
+			}
+		}
+		}
+	
